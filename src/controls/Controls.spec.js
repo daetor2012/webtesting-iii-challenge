366dab55789b68f4e6862442925e2705ad0bd163
daetor2012 <53632554+daetor2012@users.noreply.github.com>
2020-01-15 22:11:27 -0500

    Finished

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,16 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Dashboard from "../dashboard/Dashboard";
+
 // Test away!
+
+test('Buttons work', () => {
+    const { getByText } = render(<Dashboard />);
+    getByText(/lock gate/i);
+    fireEvent.click(getByText(/close gate/i));
+    expect(getByText(/lock gate/i).disabled).toBe(false);
+    fireEvent.click(getByText(/lock gate/i));
+    getByText(/unlock gate/i);
+    expect(getByText(/open gate/i).disabled).toBe(true);
+});
